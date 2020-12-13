fe09351d074eb0d6d39f92f9d13f04b378f45280
V.Tynys <ville.tynys@yahoo.com>
2020-12-13 12:18:39 +0200

    Make prettier force single quotes in jsx

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      <Link to="/home">Home</Link>
-      <Link to="/printers">Printers</Link>
-      <Link to="/history">History</Link>
-      <Link to="/settings">Settings</Link>
-      <Link to="/logout">Logout</Link>
+      <Link to='/home'>Home</Link>
+      <Link to='/printers'>Printers</Link>
+      <Link to='/history'>History</Link>
+      <Link to='/settings'>Settings</Link>
+      <Link to='/logout'>Logout</Link>
     </div>
   );
 };
