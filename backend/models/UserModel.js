303862d9ec63c71be42958d4bb305c455cf1c9e1
V.Tynys <ville.tynys@yahoo.com>
2020-12-16 14:31:22 +0200

    Create UserModel and password hashing

---
+++ backend/models/UserModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    default: Date.now,
+  },
+});
+
+//compiling the schema into a model
+module.exports = mongoose.model('UserModel', UserSchema);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 const User = require('../models/UserModel');
 
 const signinValidator = [
   check('email', 'Please add email').not().isEmpty(),
-  check('email', 'Please include a valid email').isEmail(), //error checking with express-validator
+  check('email', 'Please include a valid email').isEmail(),
   check(
     'password',
     'Please enter a password with 6 or more characters'
@@ -15,12 +16,34 @@ const signinValidator = [
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', signinValidator, (req, res) => {
+router.post('/', signinValidator, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({ errors: errors.array() }); //returns []
   }
-  res.send('pass');
+
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email: email });
+    if (user) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+
+    //create mongoose document based on User model
+    user = new User({
+      email: email,
+      password: password,
+    });
+
+    //hash (password with salt) and save user to db
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+    await user.save(); //update db
+    res.send(user.password);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server error');
+  }
 });
 
 module.exports = router;
