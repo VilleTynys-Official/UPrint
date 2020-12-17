400ffebd1b0879b0a7016f17261bf024a30b9591
V.Tynys <ville.tynys@yahoo.com>
2020-12-17 23:18:20 +0200

    Simplified routing

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
 const connectDB = require('./config/db');
 const app = express();
+const authRoutes = require('./routes/authRoutes');
+const fileRoutes = require('./routes/fileRoutes');
+const userRoutes = require('./routes/userRoutes');
 const PORT = process.env.PORT | 5000;
 
 //CONNECT TO DATABASE
@@ -9,7 +11,11 @@ connectDB();
 
 //MIDDLEWARE
 app.use(express.json({ extended: false })); //parser
-app.use('/api', apiRoutes); //routes
+
+//ROUTES
+app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/files', fileRoutes);
 
 app.get('/', (req, res) => {
   console.log();
