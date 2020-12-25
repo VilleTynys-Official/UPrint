10ac62562afaaf13001f6d90ba844d77a78ff9d8
V.Tynys <ville.tynys@yahoo.com>
2020-12-25 21:18:49 +0200

    Create Header and Footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,33 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/Navbar/NavBar';
+import Navbar from './components/layout/Navbar/Navbar';
 import HomePage from './components/HomePage/HomePage';
 import { Switch, Route } from 'react-router-dom';
 import PrintersPage from './components/PrintersPage/PrintersPage';
 import SettingsPage from './components/SettingsPage/SettingsPage';
 import HistoryPage from './components/HistoryPage/HistoryPage';
-import Header from './components/Header/Header';
+import Header from './components/layout/Header/Header';
+import Footer from './components/layout/Footer/Footer';
 
 const App = () => {
   return (
     <div className='App-container'>
-      <div className='NavBar-Main-container'>
-        <div className='NavBar-container'>
-          <NavBar></NavBar>
+      <div className='Navbar-Main-container'>
+        <div className='Navbar-container'>
+          <Navbar></Navbar>
         </div>
       </div>
       <div className='Mother-container'>
         <Header></Header>
         <div className='Main'>
           <Switch>
-            <Route path='/home'>
-              <HomePage></HomePage>
-            </Route>
-            <Route path='/printers'>
-              <PrintersPage></PrintersPage>
-            </Route>
-            <Route path='/settings'>
-              <SettingsPage></SettingsPage>
-            </Route>
-            <Route path='/history'>
-              <HistoryPage></HistoryPage>
-            </Route>
+            <Route path='/home' component={HomePage}></Route>
+            <Route path='/printers' component={PrintersPage}></Route>
+            <Route path='/settings' component={SettingsPage}></Route>
+            <Route path='/history' component={HistoryPage}></Route>
           </Switch>
         </div>
-        <div className='Footer'>
-          <p>Footer</p>
-        </div>
+        <Footer></Footer>
       </div>
     </div>
   );
---
+++ frontend/src/components/layout/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Footer = () => {
+  return (
+    <div className='Footer'>
+      <p>
+        This is a footer. Here could be information about the company and other
+        links.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
