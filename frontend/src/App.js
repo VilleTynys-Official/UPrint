d5d8dd3bc3bb1173c5fdd8f1c0133a2ebb149518
V.Tynys <ville.tynys@yahoo.com>
2020-12-28 11:02:52 +0200

    Remove theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar/Navbar';
 import HomePage from './components/HomePage/HomePage';
@@ -8,27 +8,35 @@ import SettingsPage from './components/SettingsPage/SettingsPage';
 import HistoryPage from './components/HistoryPage/HistoryPage';
 import Header from './components/layout/Header/Header';
 import Footer from './components/layout/Footer/Footer';
+import FileState from './context/file/FileState';
 
 const App = () => {
-  return (
-    <div className='App-container'>
-      <div className='Navbar-Main-container'>
+  const [isLoggedin, setLoggedin] = useState(false);
+  console.log('is user logged in: ', isLoggedin);
+
+  return isLoggedin ? (
+    <FileState>
+      <div className='App-container'>
         <div className='Navbar-container'>
           <Navbar></Navbar>
         </div>
-      </div>
-      <div className='Mother-container'>
-        <Header></Header>
-        <div className='Main'>
-          <Switch>
-            <Route path='/home' component={HomePage}></Route>
-            <Route path='/printers' component={PrintersPage}></Route>
-            <Route path='/settings' component={SettingsPage}></Route>
-            <Route path='/history' component={HistoryPage}></Route>
-          </Switch>
+        <div className='Mother-container'>
+          <Header></Header>
+          <div className='Main'>
+            <Switch>
+              <Route exact path='/home' component={HomePage}></Route>
+              <Route exact path='/printers' component={PrintersPage}></Route>
+              <Route exact path='/settings' component={SettingsPage}></Route>
+              <Route exact path='/history' component={HistoryPage}></Route>
+            </Switch>
+          </div>
+          <Footer></Footer>
         </div>
-        <Footer></Footer>
       </div>
+    </FileState>
+  ) : (
+    <div>
+      <button onClick={() => setLoggedin(true)}>Login</button>
     </div>
   );
 };
