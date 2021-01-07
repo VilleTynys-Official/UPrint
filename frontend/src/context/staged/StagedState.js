168d1ee1ac0705b495db96ee26a2427b3702de3b
VilleTynysCloubi <ville.tynys@cloubi.com>
2021-01-07 18:42:35 +0200

    Modify StagedState default values and add clarification

--- frontend/src/context/staged/StagedState.js
+++ frontend/src/context/staged/StagedState.js
@@ -6,12 +6,20 @@ import StagedContext from './StagedContext';
  * This Context holds the state for the controlled modal form.
  * Basically, stagedFile collects all the printing settings and filedata.
  * When user is satisfied and clicks save, stagedFile is used to update the FileState.
+ *
+ * The state is separated here because it is used by at least two components at the moment (AddFileModal & FileDropZone)
  */
 
 const StagedState = props => {
   const [stagedFile, setStagedFile] = useState({
-    name: 'This is a filename placeholder',
-    data: '12342'
+    date: '',
+    readyToPrint: true,
+    _id: '',
+    user: '',
+    filename: '',
+    uri: '',
+    settings: 'a4',
+    size: 0
   });
 
   return (
