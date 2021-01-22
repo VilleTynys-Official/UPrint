55936b3740122aea294ea3dbabb9e1fd915eb5ec
V.Tynys <ville.tynys@yahoo.com>
2021-01-22 10:05:49 +0200

    Fix the logic of when to show loading
    It was not working correctly and now the code is more clear.

--- frontend/src/components/Files/Files.js
+++ frontend/src/components/Files/Files.js
@@ -21,28 +21,27 @@ const Files = () => {
 
   return (
     <div>
-      {files !== null && !loading ? (
+      {loading ? (
+        <p>Loading files..</p>
+      ) : (
         <>
           <FileModal
             modalOpen={modalOpen}
             setModalOpen={setModalOpen}
           ></FileModal>
           <FileDropZone setModalOpen={setModalOpen}></FileDropZone>
-
-          {files !== null && files.length === 0 && !loading ? (
-            <h3 style={{ marginLeft: '40px' }}>
-              You can add printable files by clicking add button!
-            </h3>
-          ) : (
+          {files ? (
             <div className='Files-container'>
               {files.map(file => (
                 <FileItem key={file._id} file={file}></FileItem>
               ))}
             </div>
+          ) : (
+            <h3 style={{ marginLeft: '40px' }}>
+              You can add printable files by clicking add button!
+            </h3>
           )}
         </>
-      ) : (
-        <p>Loading..</p>
       )}
     </div>
   );
