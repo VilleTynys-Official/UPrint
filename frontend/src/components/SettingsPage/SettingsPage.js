ad823aecb592ad9d2b3d45822db404cda4d23c5d
V.Tynys <ville.tynys@yahoo.com>
2021-01-22 10:07:17 +0200

    Scaffold for showing user info

--- frontend/src/components/SettingsPage/SettingsPage.js
+++ frontend/src/components/SettingsPage/SettingsPage.js
@@ -1,7 +1,17 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { useContext } from 'react';
+import AuthContext from '../../context/auth/AuthContext';
 
 const SettingsPage = () => {
-  return <p>This is SettingsPage</p>;
+  const authContext = useContext(AuthContext);
+  const { user } = authContext;
+
+  return (
+    <div>
+      <h3>Logged in as:</h3>
+      <p>{user.email}</p>
+    </div>
+  );
 };
 
 export default SettingsPage;
